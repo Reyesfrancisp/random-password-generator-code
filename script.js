e131c54917956d59dcb5a64c9c211726b7f26d61
Francis Reyes <reyesfrancisp@gmail.com>
2023-06-01 12:07:12 -0400

    added arrays and prompts

--- script.js
+++ script.js
@@ -37,6 +37,10 @@ for (var upperIndex = 65; upperIndex <= 90; upperIndex++)
 
 var specialCharArray = ['!', '#', '$', '%', '&', '*', '+', '-', '@', '^'];
 
+
+function generatePassword()
+{
+
 var passwordArray = "";
 var passwordLength = 0;
 var lowercaseFlag = false;
@@ -44,20 +48,46 @@ var uppercaseFlag = false;
 var numberFlag = true;
 var specialCharFlag = false;
 
+//get password length
+
+
+//get each criteria by setting flags which allow entry
+lowercaseFlag = confirm("Do you want lower case letters in your password?");
+
+uppercaseFlag = confirm("Do you want upper case letters in your password?");
+
+numberFlag = confirm("Do you want numbers in your password?");
+
+specialCharFlag = confirm("Do you want special characters in your password?");
+
+//get the password length
+passwordLength = prompt("Please enter a number between 8 and 128 inclusive for the length of your password: ");
+
+//password length checker
+for (var flag = false; flag == false; )
+{
 if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) )
 {
 //error
-
+passwordLength = prompt("Please re-enter a number between 8 and 128 inclusive for the length of your password: ");
 }
 else
 {
 //proceed with code
+flag = true;
+}
+}
+
+//password generator itself
+
+for (var arraySelect = true; arraySelect == true;  )
+{
+  
 
 }
 
 
-
-generateRandomArrayElement(arrayContent, length)
+function generateRandomArrayElement(arrayContent, length)
 {
 var random = Math.random();
 var randomValue = Math.floor(random * length);
@@ -74,7 +104,7 @@ passwordArray += arrayContent[randomValue];
 
 // console.log(specialCharArray);
 
-
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
